44baccc4e833600bb06d54bc2c74ba1805e311de
Daniel Andersson <d.andersson86@icloud.com>
2024-10-11 16:49:33 +0200

    register users routes module in api_v1.py router module and add descriptive docstrings

--- src/api/api_v1.py
+++ src/api/api_v1.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 """
 API v1 Router
+
+This module defines the API v1 router for the FastAPI application. It
+includes all routes interacting with the PostgreSQL database. The router
+is included in the main FastAPI application instance in the src/main.py
+module.
 """
 
 from fastapi import APIRouter, Depends
+
+from api.v1_routes import user_routes
 from core.auth import get_api_key
 
 api_v1_router = APIRouter(
@@ -14,4 +22,10 @@ api_v1_router = APIRouter(
     responses={
         404: {"description": "Not found"}
     },
+)
+
+api_v1_router.include_router(
+    user_routes.router,
+    prefix="/users",
+    tags=["users"]
 )
