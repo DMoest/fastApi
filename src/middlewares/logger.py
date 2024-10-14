d8f792880a3e49a223e63d9932fd7e6eb8f27025
Daniel Andersson <d.andersson86@icloud.com>
2024-10-14 16:42:18 +0200

    update logger.py middleware module to use init_logger function to access the configured app logger

--- src/middlewares/logger.py
+++ src/middlewares/logger.py
@@ -1,20 +1,15 @@
-# src/utils/logger.py
-# !/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 """
-Logger utility module for the FastAPI application.
+This module contains the logger configuration for the FastAPI application.
 """
-
-import logging
-import os
-from datetime import datetime
-
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.requests import Request
+from starlette.responses import Response
 
-from src.core.config import get_settings
+from core.config import get_settings
+from core.logger_config import init_logger
 
 
 class LoggerMiddleware(BaseHTTPMiddleware):
@@ -23,33 +18,35 @@ class LoggerMiddleware(BaseHTTPMiddleware):
     """
 
     def __init__(self, app):
-        env_settings = get_settings()
+        """
+        Constructor method for the LoggerMiddleware class
+
+        :param app: FastAPI application instance
+        :type app: FastAPI
+        """
+        settings = get_settings()
+
         super().__init__(app)
-        self.logger = logging.getLogger(env_settings.app_log_name)
-        self.logger.setLevel(env_settings.app_log_level)
+        self.logger = init_logger(settings.console_logger_name)
 
-        formatter = logging.Formatter(fmt=env_settings.app_log_format)
+    async def dispatch(self, request: Request, call_next) -> Response:
+        """
+        Log the request and response
 
-        stream_handler = logging.StreamHandler()
-        stream_handler.setFormatter(formatter)
-
-        log_dir = env_settings.app_log_file_dir
-        if not os.path.exists(log_dir):
-            os.makedirs(log_dir)
-
-        log_file = os.path.join(
-            log_dir,
-            f'{datetime.date(datetime.utcnow())}_'
-            f'{env_settings.app_log_name}.log'
-        )
-        file_handler = logging.FileHandler(log_file)
-        file_handler.setFormatter(formatter)
-
-        self.logger.addHandler(stream_handler)
-        self.logger.addHandler(file_handler)
-
-    async def dispatch(self, request: Request, call_next):
+        :param request: Request object
+        :type request: Request
+        :param call_next: Next middleware to call
+        :type call_next: Callable
+        :return: Response object
+        :rtype: Response
+        """
+        # Log the request
         self.logger.info("Request: %s %s", request.method, request.url)
+
+        # Call the next middleware
         response = await call_next(request)
+
+        # Log the response
         self.logger.info("Response: %s", response.status_code)
+
         return response
