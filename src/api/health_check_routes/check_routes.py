b0f9af02f2720b77debce9f8f66ee8ab91860fc5
Daniel Andersson <d.andersson86@icloud.com>
2025-01-10 12:59:25 +0100

    add check_routes.py router module for a health checker route

---
+++ src/api/health_check_routes/check_routes.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+Utility routes for the FastAPI application.
+"""
+
+import logging
+
+from fastapi import APIRouter, status
+from fastapi.responses import ORJSONResponse
+
+from src.core.env_config import get_settings
+
+health_check_router = APIRouter()
+settings = get_settings()
+logger = logging.getLogger(settings.app_logger_name or "application_logger")
+
+
+# Health check endpoint
+@health_check_router.get("")
+async def health_check() -> ORJSONResponse:
+    """
+    Health check endpoint to verify the application status
+
+    :return: JSON response with status code 200
+    :rtype: ORJSONResponse
+    """
+    logger.info("Health check endpoint called...")
+    return ORJSONResponse(
+        status_code=status.HTTP_200_OK,
+        content="Server startup healthy..."
+    )
