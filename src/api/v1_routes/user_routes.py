184bf83237894ee1325f31067da77ee52e5e8519
Daniel Andersson <d.andersson86@icloud.com>
2025-01-17 10:41:42 +0100

    update user_routes.py

--- src/api/v1_routes/user_routes.py
+++ src/api/v1_routes/user_routes.py
@@ -18,22 +18,20 @@ from starlette.responses import Response
 from src.core.custom_exceptions import ConflictException, \
     InternalServerException, NotFoundException
 from src.core.env_config import get_settings
-from src.db.connectors.postgres_db import get_db
+from src.db.connectors.postgres_db import get_pg_db
 from src.db.models.v1_models.users_model import UserModel
 from src.db.schemas.v1_schemas.user_schemas import UserCreate, UserOutput
 
 # Initialize the API router
 router = APIRouter()
 
-# Initialize settings from environment configuration
+# Initialize environment settings & logger
 settings = get_settings()
-
-# Initialize the logger
 logger = logging.getLogger(
     settings.app_logger_name or "application_logger")
 
 
-@router.options("")
+@router.options("", operation_id="options_v1_user_routes")
 def options_user_routes() -> Response:
     """
     Handles OPTIONS requests for the /users endpoint.
@@ -44,11 +42,15 @@ def options_user_routes() -> Response:
     )
 
 
-@router.post("", response_class=ORJSONResponse,
+@router.post("",
+             name="create_user_v1_in_pg_db",
+             description="Create a new User",
+             operation_id="create_user_v1_in_pg_db",
+             response_class=ORJSONResponse,
              status_code=status.HTTP_201_CREATED)
 async def create_user(
         user: UserCreate,
-        db: AsyncSession = Depends(get_db)) -> ORJSONResponse:
+        db: AsyncSession = Depends(get_pg_db)) -> ORJSONResponse:
     """
     Create a new User.
     """
@@ -71,9 +73,11 @@ async def create_user(
         raise InternalServerException(message="Internal Server Error") from e
 
 
-@router.get("", response_class=ORJSONResponse)
+@router.get("",
+            response_class=ORJSONResponse,
+            operation_id="get_all_users_v1_in_pg_db")
 async def get_all_users(
-        db: AsyncSession = Depends(get_db)) -> ORJSONResponse:
+        db: AsyncSession = Depends(get_pg_db)) -> ORJSONResponse:
     """
     Retrieve all Users that are not soft-deleted.
     """
